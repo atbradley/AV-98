d7082d23e5c9744ed29ee15ff9c631b7b599eb57
Solderpunk <solderpunk@sdf.org>
2020-06-07 22:48:44 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='AV-98',
+    version='0.0.1',
+    description="Command line Gemini client.",
+    author="Solderpunk",
+    author_email="solderpunk@sdf.org",
+    url='https://tildegit.org/solderpunk/AV-98/',
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Communications',
+        'Intended Audience :: End Users/Desktop',
+        'Environment :: Console',
+        'Development Status :: 4 - Beta',
+    ],
+    py_modules = ["av98"],
+    entry_points={
+        "console_scripts": ["av98=av98:main"]
+    },
+    install_requires=[],
+)
